83ffb5992f19a5774e1f7fcb5626c96c0dd5a4ef
Widya Walesa <walecha99@gmail.com>
2018-06-10 23:14:38 +0700

    msodbcsql17: Updated.
    * Use RHEL msodbcsql17 and openssl-libs
    * Add msodbcsql17 libdir to /etc/ld.so.conf
    
    Signed-off-by: Widya Walesa <walecha99@gmail.com>

--- msodbcsql17/doinst.sh
+++ msodbcsql17/doinst.sh
@@ -1,5 +1,11 @@
 
 #=============================#
+if [ -z $(grep "/opt/microsoft/msodbcsql17/lib64" /etc/ld.so.conf) ]; then
+  echo "Spamming /etc/ld.so.conf"
+  echo "/opt/microsoft/msodbcsql17/lib64" >>/etc/ld.so.conf
+  /sbin/ldconfig
+fi
+
 echo "Install msodbcsql17 driver with this command (run as root):"
 echo "  odbcinst -i -s -f /opt/microsoft/msodbcsql17/etc/odbcinst.ini -l"
 echo
