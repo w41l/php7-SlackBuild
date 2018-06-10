23de1d9d4645e80ac54b5ad731e838c76dbd779f
Widya Walesa <walecha99@gmail.com>
2018-06-10 19:06:54 +0700

    msodbcsql17: Added version 17.1.0.1
    Signed-off-by: Widya Walesa <walecha99@gmail.com>

---
+++ msodbcsql17/doinst.sh
@@ -0,0 +1,5 @@
+
+#=============================#
+echo "Install msodbcsql17 driver with this command (run as root):"
+echo "  odbcinst -i -s -f /opt/microsoft/msodbcsql17/etc/odbcinst.ini -l"
+echo
